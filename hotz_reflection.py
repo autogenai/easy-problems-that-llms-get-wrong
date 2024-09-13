7b74ee31da6d3f09b6749ca7ebf826986f7e74ce
Ray Bernard <ray.bernard@outlook.com>
2024-09-13 18:02:05 -0400

    fixed typo

--- hotz_reflection.py
+++ hotz_reflection.py
@@ -8,7 +8,7 @@ INITIAL ANSWER
 {question["model_answer"]}
 
 REFLECTION TASK
-Review the question carfully and assess your initial answer. You can amend the answer if you wish too, otherwise return the original answer. Return in JSON format, for example:
+Review the question carefully and assess your initial answer. You can amend the answer if you wish too, otherwise return the original answer. Return in JSON format, for example:
 {{"ANSWER": {random.choice(['A','B','C','D'])}}}
 """
 
